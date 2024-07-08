691b0ce122c0fa9ddfd21ca96433996e1f694dee
Zholborsbek Ysmaiyl uulu <63614518+Zhobk@users.noreply.github.com>
2024-07-08 13:03:29 +0600

    .

--- src/lib/exportzip.js
+++ src/lib/exportzip.js
@@ -4,42 +4,37 @@ import JSZip from 'jszip';
 export async function exportBookAsZip(book) {
   const zip = new JSZip();
 
-  // Create folders for each type inside 'data'
   const folders = ['books', 'images', 'videos', 'models', 'ppt', /* 'survey' */];
   folders.forEach(folder => {
-    zip.folder(`data/${folder}`); // Nested inside 'data' folder
+    zip.folder(`data/${folder}`); 
   });
 
-  const sanitizedTitle = sanitizeFileName(book.title); // Sanitize title for filename
+  const sanitizedTitle = sanitizeFileName(book.title); 
   zip.folder('data/books').file(`${sanitizedTitle}.json`, JSON.stringify(book));
 
-  // Function to sanitize filename
   function sanitizeFileName(fileName) {
-    // Replace invalid characters in filename
     return fileName.replace(/[<>:"/\\|?*]/g, '_');
   }
 
-  // Array to store all fetch promises
   const fetchPromises = [];
 
-  // Function to resolve paths based on packaging status
   const resolvePath = (path) => {
-    const isPackaged = process.env.NODE_ENV === 'production'; // Check if packaged
+    const isPackaged = process.env.NODE_ENV === 'production'; 
      const resourcesPath = process.env.NODE_ENV === 'resourcesPath';
     const resolvedPath = isPackaged ? path.replace(resourcesPath, '/resources/data/', 'resources/data/') : path;
     return resolvedPath;
   };
 
-  // Function to resolve file name based on path
+
   const resolveFileName = (path, baseFolder) => {
-    const isPackaged = process.env.NODE_ENV === 'production'; // Check if packaged
+    const isPackaged = process.env.NODE_ENV === 'production'; 
     const relativePath = isPackaged ? path.split(`../resources/data/${baseFolder}/`)[1] : path.split(`/src/data/${baseFolder}/`)[1];
     const fileName = `data/${baseFolder}/${relativePath}`;
     return fileName;
     
   };
 
-  // Add images from chapters to the 'images' folder
+
   if (book.chapters) {
     book.chapters.forEach(chapter => {
       if (chapter.blocks) {
@@ -64,7 +59,7 @@ export async function exportBookAsZip(book) {
     });
   }
 
-  // Add videos from chapters to the 'videos' folder
+
   if (book.chapters) {
     book.chapters.forEach(chapter => {
       if (chapter.blocks) {
@@ -87,7 +82,7 @@ export async function exportBookAsZip(book) {
     });
   }
 
-  // Add models from chapters to the 'models' folder
+
   if (book.chapters) {
     book.chapters.forEach(chapter => {
       if (chapter.blocks) {
@@ -110,7 +105,7 @@ export async function exportBookAsZip(book) {
     });
   }
 
-  // Add PowerPoint files from chapters to the 'ppt' folder
+
   if (book.chapters) {
     book.chapters.forEach(chapter => {
       if (chapter.blocks) {
@@ -131,14 +126,13 @@ export async function exportBookAsZip(book) {
     });
   }
 
-  // Wait for all fetches to complete before generating the zip
+
   await Promise.all(fetchPromises);
 
-  // Generate the zip file
+
   const zipBlob = await zip.generateAsync({ type: 'blob' });
 
-  // Save the zip file using FileSaver.js or any other method
   saveAs(zipBlob, `${sanitizedTitle}`);
 
-  return zipBlob; // Optional: Return the zip blob if needed
+  return zipBlob; 
 }
